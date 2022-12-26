e7c3e3438da3659b65bca562b6e9e8e5e743f277
Nancy Bou ghannam <bgnancy994@hotmail.com>
2022-12-26 15:55:23 +0200

    Added Database Tables and Migrations, End Implementation Database Section

---
+++ LeaveManagement.Web/Data/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateJoined { get; set; }
+    }
+}
---
+++ LeaveManagement.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Data
+{
+    public class LeaveAllocation:BaseEntity
+    {
+        public int NumberOfDate { get; set; }
+        //how to create a foreign key 
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int EmployeeId { get; set; }
+    }
+}
